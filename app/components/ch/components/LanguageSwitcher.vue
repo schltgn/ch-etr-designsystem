6ef1cf27b4674c4deafb9cae2a83d4ec9ece89e3
Florian Schlittgen <schlittgen@liwa.de>
2025-01-31 09:22:39 +0100

    Handles change event of language switcher

--- app/components/ch/components/LanguageSwitcher.vue
+++ app/components/ch/components/LanguageSwitcher.vue
@@ -1,10 +1,11 @@
 <template>
   <div :class="computedClass">
     <label for="lang-switcher" class="sr-only"> Select language </label>
-    <Select id="lang-switcher" size="sm" bare :variant="type">
+    <Select id="lang-switcher" size="sm" bare :variant="type" @change="changeLanguage">
       <option
-          v-for="lang in $languages"
+          v-for="lang in $clientData.languages"
           :key="lang.text"
+          :value="lang.url"
           :selected="lang.active"
       >
         {{ lang.text }}
@@ -28,4 +29,10 @@ const computedClass = computed(() => {
   const base = 'language-switcher'
   return base
 })
+
+const changeLanguage = (url) => {
+  if (url) {
+    window.location.href = url;
+  }
+}
 </script>
