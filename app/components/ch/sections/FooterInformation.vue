29c0432df5a5f8d3ec5550d7662f5e4cbf4e8e0c
Florian Schlittgen <schlittgen@liwa.de>
2025-02-05 08:57:28 +0100

    Remove content from FooterInformation

--- app/components/ch/sections/FooterInformation.vue
+++ app/components/ch/sections/FooterInformation.vue
@@ -3,97 +3,8 @@
     <div class="container">
       <div class="footer-information">
         <div class="footer-information__entry">
-          <h3>Über uns</h3>
-          <p>
-            In vitae risus porttitor, imperdiet urna quis, venenatis mi. Aliquam
-            mattis nibh orci, a pellentesque diam rutrum vel. In vitae risus
-            porttitor, imperdiet urna quis, venenatis mi. Aliquam mattis nibh
-            orci, a pellentesque diam rutrum vel.
-          </p>
-          <p>© Geoportal</p>
-        </div>
-        <div class="footer-information__entry">
-          <h3>Bleiben sie informiert</h3>
-          <div class="footer-information__social">
-            <a class="footer__link" href="facebook.com">
-              <SvgIcon icon="Facebook" class="footer-information__icon" />
-              Facebook
-            </a>
-            <a href="twitter.com" class="footer__link">
-              <SvgIcon icon="Twitter" class="footer-information__icon" />
-              Twitter
-            </a>
-            <a href="youtube.com" class="footer__link">
-              <SvgIcon icon="Youtube" class="footer-information__icon" />
-              Youtube
-            </a>
-            <a href="linkedin.com" class="footer__link">
-              <SvgIcon icon="LinkedIn" class="footer-information__icon" />
-              LinkedIn
-            </a>
-            <a href="xing.com" class="footer__link">
-              <SvgIcon icon="Xing" class="footer-information__icon" />
-              Xing
-            </a>
-          </div>
-
-          <Btn
-            label="News abonnieren"
-            icon="ArrowRight"
-            icon-pos="right"
-            variant="outline-negative"
-          />
-        </div>
-        <div class="footer-information__entry footer-information__entry--big">
-          <h3>Weitere Informationen</h3>
-          <div class="footer-information__links">
-            <div class="footer-information__links-column">
-              <a
-                href="javascript:void(0)"
-                class="footer__link footer-information__link--icon-right"
-              >
-                <SvgIcon icon="ArrowRight" class="footer-information__icon" />
-                Über uns
-              </a>
-              <a
-                href="javascript:void(0)"
-                class="footer__link footer-information__link--icon-right"
-              >
-                <SvgIcon icon="ArrowRight" class="footer-information__icon" />
-                Jobs
-              </a>
-              <a
-                href="javascript:void(0)"
-                class="footer__link footer-information__link--icon-right"
-              >
-                <SvgIcon icon="ArrowRight" class="footer-information__icon" />
-                Kontakt
-              </a>
-            </div>
-            <div class="footer-information__links-column">
-              <a
-                href="javascript:void(0)"
-                class="footer__link footer-information__link--icon-right"
-              >
-                <SvgIcon icon="ArrowRight" class="footer-information__icon" />
-                Medien
-              </a>
-              <a
-                href="javascript:void(0)"
-                class="footer__link footer-information__link--icon-right"
-              >
-                <SvgIcon icon="ArrowRight" class="footer-information__icon" />
-                Barrierefreiheit
-              </a>
-              <a
-                href="javascript:void(0)"
-                class="footer__link footer-information__link--icon-right"
-              >
-                <SvgIcon icon="ArrowRight" class="footer-information__icon" />
-                Impressum
-              </a>
-            </div>
-          </div>
+          <h3 v-html="$clientData.t('footer.contact.heading')" />
+          <p v-html="$clientData.t('layout.footer')" />
         </div>
       </div>
     </div>
@@ -101,6 +12,4 @@
 </template>
 
 <script setup lang="ts">
-import SvgIcon from '../components/SvgIcon.vue'
-import Btn from '../components/Btn.vue'
 </script>
