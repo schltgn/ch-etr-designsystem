246bb6db37bb3a01d3039c1501c1855b0d8caf9f
Florian Schlittgen <schlittgen@liwa.de>
2025-06-24 16:47:43 +0200

    Amend footer

--- app/components/ch/sections/FooterInformation.vue
+++ app/components/ch/sections/FooterInformation.vue
@@ -6,10 +6,27 @@
           <h3 v-html="$clientData.t('footer.contact.heading')" />
           <p v-html="$clientData.t('layout.footer')" />
         </div>
+
+        <div class="footer-information__entry">
+          <h3>{{ $clientData.t('layout.footer.headingLinks') }}</h3>
+          <div class="footer-information__links">
+            <div class="footer-information__links-column">
+              <a
+                v-for="(item) in $clientData.footer1Navigation"
+                :href="item.url"
+                class="footer__link footer-information__link--icon-right"
+              >
+                <SvgIcon icon="ArrowRight" class="footer-information__icon" />
+                {{ item.text }}
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import SvgIcon from '../components/SvgIcon.vue'
 </script>
--- app/components/ch/sections/FooterNavigation.vue
+++ app/components/ch/sections/FooterNavigation.vue
@@ -3,19 +3,20 @@
     <nav class="container" aria-label="Footer">
       <ul class="footer-navigation">
         <li
-          v-for="(item, index) in $clientData.footerNavigation"
+          v-for="(item, index) in $clientData.footer2Navigation"
           :class="{
-            'mr-auto': index === $clientData.footerNavigation.length - 1,
+            'mr-auto': index === $clientData.footer2Navigation.length - 1,
           }"
         >
           <a :href="item.url" class="footer__link">{{ item.text }}</a>
         </li>
-        <li class="footer__label ml-auto" v-if="$clientData.personIdentifier != null">
+        <li class="footer__label ml-auto">{{ $clientData.currentDateTime }}</li>
+        <li class="footer__label" v-if="$clientData.personIdentifier != null">
           {{ $clientData.personIdentifier }}
         </li>
-        <li class="footer__label">{{ $clientData.currentDateTime }}</li>
       </ul>
     </nav>
   </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+</script>
